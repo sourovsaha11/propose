18ebbefd654d7ee1cf7ce895d643d9696e099094
sourovsaha11 <sourovsahapapai0@gmail.com>
2024-06-01 12:17:55 +0600

    first commit

---
+++ love.js
@@ -0,0 +1,8 @@
+document.getElementById("yes-btn").addEventListener("click", function () {
+    const headertag = document.getElementById("h1-tag").innerText = "i love you dear 😘";
+})
+
+document.getElementById("ofcourse-yes").addEventListener("click", function () {
+    const headertag = document.getElementById("h1-tag").innerText = "i love you onkgula 😘😘";
+})
+
